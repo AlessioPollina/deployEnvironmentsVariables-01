ad6a84ca4f65cf4413e150d816182736c5861eac
Alessio Pollina <alexpollina@libero.it>
2023-03-06 15:37:14 +0100

    Initial commit

---
+++ src/test/java/co/develhope/deployEnvironmentsVariables01/DeployEnvironmentsVariables01ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.deployEnvironmentsVariables01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployEnvironmentsVariables01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
